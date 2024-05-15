073a78d9743ca5bffda47629e95d7a7590ac7d8c
Rh4096 <rhelvetican@gmail.com>
2024-05-16 05:50:15 +0700

    add ultra option

--- src/utils/effect.rs
+++ src/utils/effect.rs
@@ -1,13 +1,17 @@
 use crate::utils::{blur::BlurOpts, noise::NoiseOpts};
 use clap::ValueEnum;
 use image::DynamicImage;
-use std::{cmp::min, fmt};
+use std::{
+    cmp::min,
+    fmt::{Display, Formatter, Result},
+};
 
 #[derive(ValueEnum, Debug, Copy, Clone)]
 pub enum EffectStrength {
     Low,
     Medium,
     High,
+    Ultra,
 }
 
 impl EffectStrength {
@@ -44,16 +48,27 @@ impl EffectStrength {
                     seed: (min_dim as f64 * 0.01) as u64,
                 },
             ),
+            EffectStrength::Ultra => (
+                BlurOpts {
+                    sigma: min_dim as f32 * 0.08,
+                },
+                NoiseOpts {
+                    mean: 0.00f64,
+                    std_dev: min_dim as f64 * 0.0125,
+                    seed: (min_dim as f64 * 0.01) as u64,
+                },
+            ),
         }
     }
 }
 
-impl fmt::Display for EffectStrength {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl Display for EffectStrength {
+    fn fmt(&self, f: &mut Formatter) -> Result {
         match self {
             EffectStrength::Low => write!(f, "low"),
             EffectStrength::Medium => write!(f, "medium"),
             EffectStrength::High => write!(f, "high"),
+            EffectStrength::Ultra => write!(f, "ultra"),
         }
     }
 }
